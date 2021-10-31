fab070668ea30ce76e90c638c90b3659047598c5
sj <ysj13@kakao.com>
2021-10-31 21:40:19 +0900

    Summer/Winter Coding(~2018) 12982 예산

---
+++ src/Problem12982.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class Problem12982 {
+
+    class Solution {
+        public int solution(int[] d, int budget) {
+            int answer = 0;
+            int sum = 0;
+
+            Arrays.sort(d);
+
+            if(d.length > 1) {
+                for(int i = 0; i < d.length; i++) {
+                    if(budget >= sum) {
+                        sum += d[i];
+                        answer++;
+                    }
+
+                    if(sum > budget) {
+                        sum = sum - d[i-1];
+                        answer--;
+                        break;
+                    }
+                }
+            }
+
+            if(d.length == 1) {
+                if(d[0] > budget) {
+                    answer = 0;
+                } else {
+                    answer = 1;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
