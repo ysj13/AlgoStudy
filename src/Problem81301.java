4be0bdbc33f305c006f8efd9c6c1b7dd29a16416
sj <ysj13@kakao.com>
2021-10-31 21:55:32 +0900

    2021 카카오 채용연계형 인턴십
    81301 숫자 문자열과 영단어

---
+++ src/Problem81301.java
@@ -0,0 +1,17 @@
+public class Problem81301 {
+    class Solution {
+        public int solution(String s) {
+            int answer = 0;
+            String[] num = {"zero", "one", "two", "three", "four", "five",
+                    "six", "seven", "eight", "nine", "ten"};
+
+            for(int i = 0; i < num.length; i++) {
+                s = s.replace(num[i], Integer.toString(i));
+            }
+
+            answer = Integer.parseInt(s);
+
+            return answer;
+        }
+    }
+}
