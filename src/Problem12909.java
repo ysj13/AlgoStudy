6dbc590f83e4b50276a4a01e2ee0e79db1719add
sj <ysj13@kakao.com>
2021-11-03 23:38:53 +0900

    스택/큐
    12909 올바른 괄호

---
+++ src/Problem12909.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class Problem12909 {
+    public static void main(String[] args) {
+        String s = "(())()";
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+
+        if(s.charAt(0) == ')') {
+            answer = false;
+        } else {
+            for(int i = 0; i < s.length(); i++) {
+                if(s.charAt(i) == '(') {
+                    stack.push(s.charAt(i));
+                }
+
+                if(s.charAt(i) == ')' && !stack.empty()) {
+                    stack.pop();
+                }
+            }
+        }
+
+        if(!stack.empty()) {
+            answer = false;
+        }
+
+        System.out.println(answer);
+    }
+}
