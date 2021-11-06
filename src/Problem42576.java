6fde1f8226b7557f69bfb9e06061a0f5c761253d
sj <ysj13@kakao.com>
2021-11-06 23:01:17 +0900

    완주하지 못한 선수 42576

---
+++ src/Problem42576.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class Problem42576 {
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion =  {"eden", "kiki"};
+
+        System.out.println(solution(participant, completion));
+    }
+
+    public static String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String str : participant) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        for(String str : completion) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        for(String str : map.keySet()) {
+            if(map.get(str) % 2 != 0) {
+                answer = str;
+            }
+        }
+
+        return answer;
+    }
+}
