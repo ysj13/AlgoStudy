4d157401864996aeaf6f601362bd545cdd6954c3
sj <ysj13@kakao.com>
2021-11-09 21:20:31 +0900

    x만큼 간격이 있는 n개의 숫자 12954

---
+++ src/Problem12954.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Problem12954 {
+    public static void main(String[] args) {
+        int x = 10000000;
+        int n = 1000;
+
+        System.out.println(Arrays.toString(solution(x, n)));
+    }
+
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long num = x;
+
+        for(int i = 0; i < n; i++) {
+            answer[i] = x + (long) i * x;
+        }
+
+        return answer;
+    }
+}
