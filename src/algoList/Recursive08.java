f5708a1f87d7f588d55e3580ae849d553c80f299
sj <ysj13@kakao.com>
2022-03-22 00:44:58 +0900

    송아지 찾기 BFS

---
+++ src/algoList/Recursive08.java
@@ -0,0 +1,54 @@
+package algoList;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * BFS 송아지 찾기
+ */
+public class Recursive08 {
+    int answer = 0;
+    int[] dis = {1, -1, 5};
+    int[] ch;
+    Queue<Integer> Q = new LinkedList<>();
+
+    public int BFS(int s, int e) {
+        ch = new int[10001];
+        ch[s] = 1;
+        Q.offer(s);
+        int L = 0;
+
+        while (!Q.isEmpty()) {
+            int length = Q.size();
+
+            for(int i = 0; i < length; i++) {
+                int x = Q.poll();
+                if(x == e) {
+                    return L;
+                }
+                for(int j = 0; j < 3; j++) {
+                    int nx = x + dis[j];
+                    if(nx == e) {
+                        return L + 1;
+                    }
+                    if(nx >= 1 && nx <= 10000 && ch[nx] == 0) {
+                        ch[nx] = 1;
+                        Q.offer(nx);
+                    }
+                }
+            }
+            L++;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Recursive08 T = new Recursive08();
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int e = sc.nextInt();
+
+        System.out.println(T.BFS(s, e));
+    }
+}
